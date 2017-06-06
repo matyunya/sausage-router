0d7bf79e422d9dff912d6750b701a7ad646cf8db
Josh Duff <me@JoshDuff.com>
2017-06-06 15:48:51 -0500

    Run the hash-brown-router's browser tests, too

--- test/router-test.js
+++ test/router-test.js
@@ -1,5 +1,8 @@
-var allTests = require('../node_modules/hash-brown-router/test/all-tests')
+var allTests = require('hash-brown-router/test/all-tests')
+var browserTests = require('hash-brown-router/test/browser-tests')
 var sausage = require('../')
 require('tap-browser-color')()
 
-allTests(sausage())
+const router = sausage()
+allTests(router)
+browserTests(router)
